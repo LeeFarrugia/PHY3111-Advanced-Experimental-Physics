698f3726dad0c3ddf3fc24ccbb252945b291662a
Lee Farrugia <farrugialee@gmail.com>
2022-10-08 09:09:01 +0200

    Adding comments and saving figure

--- Regression Analysis/Session 1.py
+++ Regression Analysis/Session 1.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# creating the empty lists to be used
 xlist = []
 ylist = []
 prodlist = []
@@ -10,7 +11,9 @@ denomlist = []
 ydenomlist = []
 yelist = []
 
+# reading the data
 data = pd.read_csv('Q1__Youngs_Modulus_of_a_Wire.csv')
+# changing the individual columns into arrays
 diameter_array = (data['Diameter/m']).to_numpy()
 mass_array = data['m/kg'].to_numpy()
 x1_array = data['x_1/m'].to_numpy()
@@ -20,15 +23,18 @@ x4_array = data['x_4/m'].to_numpy()
 L_array = data['L/m'].to_numpy()
 x0_array = data['x_0/m'].to_numpy()
 
+# finding the x-x0 part of the equation
 for i in range(len(x1_array)-1):
     x = sum([x1_array[i], x2_array[i], x3_array[i], x4_array[i]])
     X = (x/4)-x0_array[0]
     xlist.append(abs(X))
 
+# calculating xi and xbar
 xi = np.array(xlist)**2
 xbar = np.mean(xi)
 print(xbar)
 
+# calculating yi and y bar
 for i in range(len(mass_array)-1):
     yi = mass_array[i]/xlist[i]
     ylist.append(yi)
@@ -37,6 +43,7 @@ yi = np.array(ylist)
 ybar = np.mean(yi)
 print(ybar)
 
+# defining the function to calculate alpha, beta, coefficient of correlation and determination
 def beta_alpha_function(xi, xbar, yi, ybar):
     for i in range(len(xi)):
         prod = (xi[i]-xbar)*(yi[i]-ybar)
@@ -59,35 +66,42 @@ def beta_alpha_function(xi, xbar, yi, ybar):
     R = r**2
     return beta, alpha, delta_beta, delta_alpha, r, R
 
-
+# calling previous function to get the actual values
 beta, alpha, delta_beta, delta_alpha, r, R = beta_alpha_function(xi, xbar, yi, ybar)
 
+# calculating the experimental values 
 for i in range(len(xi)):
     ye = alpha + (beta*xi[i])
     yelist.append(ye)
 ye_array = np.array(yelist)
 
+# calculating the constants for the gradient and intercept
 radius = np.average(diameter_array)
 m_constant = (8*np.pi*(radius**2))/(9.81*(L_array[0]**3))
 c_constant = 4/(L_array[0]*9.81)
 
+# finding the line of best fit to the data
 coeffs, cov = np.polyfit(xi, ye_array, 1, cov=True)
 polyfunc = np.poly1d(coeffs)
 trendline = polyfunc(xi)
 
+# calculating the young's modulus and T0
 E = coeffs[0]/m_constant
 T0 = coeffs[1]/c_constant
 delta_E = np.sqrt(cov[0][0])
 delta_T0 = np.sqrt(cov[1][1])
 print(E, delta_E, T0, delta_T0)
 
+# calculating the residuals
 residual = trendline - ybar
 
+# defining the font to be used
 plt.rcParams['font.family'] = 'STIXGeneral'
 plt.rcParams['mathtext.fontset'] = 'stix'
 plt.rcParams['font.size'] = 12
 plt.rcParams['font.weight'] = 'normal'
 
+# plotting both graph as subplots
 plt.subplot(2,1,1)
 plt.scatter(xi, yi, color='k', label='Data Points')
 plt.plot(xi, trendline, color='k', label='Trendline')
@@ -107,4 +121,5 @@ plt.grid(visible=True, which='minor', linestyle='--')
 plt.ylabel('Residuals')
 plt.xlabel('Strain')
 plt.tight_layout()
+plt.savefig('Plot1.png', dpi=800)
 plt.show()
