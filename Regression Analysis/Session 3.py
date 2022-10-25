1371975bcef48c3306c03ab822c32bc73e8585b7
Lee Farrugia <farrugialee@gmail.com>
2022-10-25 15:24:32 +0200

    Finished the calcium isotope

--- Regression Analysis/Session 3.py
+++ Regression Analysis/Session 3.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.metrics import mean_squared_error
 from scipy.optimize import curve_fit
 
@@ -38,13 +39,76 @@ for i in range(len(data_sliced)):
 index_df = np.arange(0, len(df), 1)
 df.reset_index(drop=True).set_index(index_df, inplace=True)
 
+a_uvalues = df['A']
+t_values = df['t/s']
+
+mean_ua = [np.mean(a_uvalues.iloc[index_array[i]:index_array[i+1]]) for i in range(len(index_array)-1)]
+mean_t = [np.mean(t_values.iloc[index_array[i]:index_array[i+1]]) for i in range(len(index_array)-1)]
+
+plt.figure(figsize=(7.5, 10.5))
+plt.rcParams['font.family'] = 'STIXGeneral'
+plt.rcParams['mathtext.fontset'] = 'stix'
+plt.rcParams['font.size'] = 12
+plt.rcParams['font.weight'] = 'normal'
+plt.minorticks_on()
+plt.grid(visible=True, which='major', linestyle='-')
+plt.grid(visible=True, which='minor', linestyle='--')
+plt.tight_layout()
+
+plt.scatter(np.log(mean_t), np.log(mean_ua), color='k')
+plt.savefig('3Plot1.png', dpi=800)
+plt.close()
+
 calcium_df = df[df['z']==20]
 calcium_df = df.iloc[5680:6080]
 calcium_df.reset_index(drop=True, inplace=True)
 
-half_array = np.arange(0,len(calcium_df),20)
+calcium_a = calcium_df['A'][0:20]
+calcium_log_a = np.log(calcium_df['A'][0:20])
+calcium_t = (calcium_df['t/s'][0:20])
 
-for z in range(len(calcium_df/20)):
-    for i in half_array:
-        a_2 = calcium_df.iloc[i:i+1]
-        
+def fit_func(t, thalf):
+    return (np.exp((-1 * t * np.log(2))/thalf))
+
+popt, pcov = curve_fit(fit_func, calcium_t, (calcium_a/calcium_a[0]))
+
+fitted_line = fit_func(calcium_t, popt[0])
+print(f'The half life of calcium-14 is: {popt[0]:.2E}s')
+
+# obtaining the straight line to compare values obtained
+coeffs, cov = np.polyfit(calcium_t, calcium_log_a, 1, cov=True)
+polyfunc = np.poly1d(coeffs)
+trendline = polyfunc(calcium_t)
+
+print(f'The half life of calcium-14 is from the srtaight line graph: {-np.log(2)/coeffs[0]:.2E}s')
+
+f, (a0, a1) = plt.subplots(2, 1, sharex=True, sharey=False, figsize=(7.3, 10.7))
+
+plt.rcParams['font.family'] = 'STIXGeneral'
+plt.rcParams['mathtext.fontset'] = 'stix'
+plt.rcParams['font.size'] = 12
+plt.rcParams['font.weight'] = 'normal'
+
+a0.scatter(calcium_t, (calcium_a/calcium_a[0]), color='k', label='Data Points')
+a0.plot(calcium_t, fitted_line, color='k', label='Trendline')
+a0.minorticks_on()
+a0.grid(visible=True, which='major', linestyle='-')
+a0.grid(visible=True, which='minor', linestyle='--')
+a0.set_xlabel('Time/s')
+a0.set_ylabel('A/A0')
+a0.set_title(r'A graph $\frac{A}{A_0}$ vs Time')
+
+a1.scatter(calcium_t, calcium_log_a, color='k', label='Data Point')
+a1.plot(calcium_t, trendline, color='k', label='Trendline')
+a1.minorticks_on()
+a1.grid(visible=True, which='major', linestyle='-')
+a1.grid(visible=True, which='minor', linestyle='--')
+a1.set_xlabel('Time/s')
+a1.set_ylabel(r'$log(A)$')
+a1.set_title(r'A graph of $log(A)$ vs Time')
+
+f.tight_layout()
+f.legend()
+f.savefig('3Plot2.png', dpi=800)
+plt.close()
+
