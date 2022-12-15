f354d797c7914391720b1ec50a2611ebc4b5deb5
Lee Farrugia <farrugialee@gmail.com>
2022-12-15 09:02:36 +0100

    Derivation

--- Pendulum Project/Pendulum Project.py
+++ Pendulum Project/Pendulum Project.py
@@ -1,3 +1,43 @@
 import numpy as np
-import pandas as pd
+import scipy as sc
 import matplotlib.pyplot as plt
+from math import *
+from sympy import *
+from scipy.integrate import odeint
+
+x,y,r,t,m,v,i,g= symbols('x,y,r,t,m,v,i,g')
+theta = Function('theta')(t)
+
+x = r * sin(theta)
+y = (-1*r) * cos(theta)
+theta_dot = diff(theta, t)
+
+vx = diff(x,t)
+vy = diff(y,t)
+
+vt = vx**2 + vy**2
+T_rec = 0.5* m * v.subs(v, vt)
+T_rot = 0.5* i* diff(theta,t)**2
+
+T = T_rec + T_rot
+U = m*g*r*(1 - cos(theta))
+L = T - U
+
+Q = diff(diff(L, theta_dot),t) - diff(L, theta)
+
+def ode(theta, t):
+    a = (-m*g*r*(np.sin(theta)))/(i+(m*(r**2)))
+    return a
+
+m = 1
+r  = 0.5
+i = 0.025
+theta0 = np.pi/2
+t = np.linspace(0, 10, 100)
+g = 9.81
+
+theta = odeint(ode, theta0, t)
+print(theta)
+
+plt.plot(t, theta[:,0])
+plt.show()
