c145fa596158ebe8db68781ad7714bea2fe5afa1
Lee Farrugia <farrugialee@gmail.com>
2023-04-23 14:49:11 +0200

    Final Changes

--- Galactic Dynamics/Code.py
+++ Galactic Dynamics/Code.py
@@ -4,6 +4,7 @@ import pynbody
 import pynbody.plot.sph as sph
 from functools import reduce
 
+#Question 1
 s1 = pynbody.load('run708main.01000')
 
 print(s1.families()) # Exploring groups in simulation
@@ -14,13 +15,12 @@ print(f's1.g: {s1.g.loadable_keys()}') # Showing keys for gas
 
 s2 = pynbody.load('run708mainDiff.01000')
 
-# print(s2.families()) # Exploring groups in simulation
-# print(s2.properties) # Showing the properties of the simulation
-# print(f's2.s: {s2.s.loadable_keys()}') # Showing keys for stars
-# print(f's2.d: {s2.d.loadable_keys()}') # Showing keys for dark matter
-# print(f's2.g: {s2.g.loadable_keys()}') # Showing keys for gas
+print(s2.families()) # Exploring groups in simulation
+print(s2.properties) # Showing the properties of the simulation
+print(f's2.s: {s2.s.loadable_keys()}') # Showing keys for stars
+print(f's2.d: {s2.d.loadable_keys()}') # Showing keys for dark matter
+print(f's2.g: {s2.g.loadable_keys()}') # Showing keys for gas
 
-# #Question 1
 s1.physical_units() # Changing units to correct units
 pynbody.analysis.angmom.faceon(s1)
 
@@ -28,66 +28,79 @@ pynbody.analysis.angmom.faceon(s1)
 s2.physical_units() # Changing units to correct units
 pynbody.analysis.angmom.faceon(s2)
 
-# #Question 2
-# mass_s1_s = s1.s['mass']
-# mass_s1_d = s1.d['mass']
-# mass_s1_g = s1.g['mass']
+#Question 2
+mass_s1_s = s1.s['mass'] # Extracting the mass contained within the stellar component
+mass_s1_d = s1.d['mass'] # Extracting the mass contained within the dark matter component
+mass_s1_g = s1.g['mass'] # Extracting the mass contained within the gas component
 
-# mass_s2_s = s2.s['mass']
-# mass_s2_d = s2.d['mass']
-# mass_s2_g = s2.g['mass']
+mass_s2_s = s2.s['mass'] # Extracting the mass contained within the stellar component
+mass_s2_d = s2.d['mass'] # Extracting the mass contained within the dark matter component
+mass_s2_g = s2.g['mass'] # Extracting the mass contained within the gas component
 
-# Sum = 0
-# array = []
+Sum = 0
+array = []
 
-# def mass_sum(array):
-#   Sum = reduce(lambda a, b: a+b, array)
-#   return(Sum)
+def mass_sum(array):
+  Sum = reduce(lambda a, b: a+b, array)
+  return(Sum)
 
-# total_mass_s1 = mass_sum(mass_s1_s) + mass_sum(mass_s1_d) + mass_sum(mass_s1_g)
+#C alculating the total mass contained within the stellar, dark matter, and gas components
+total_mass_s1 = mass_sum(mass_s1_s) + mass_sum(mass_s1_d) + mass_sum(mass_s1_g)
 
-# Sum = 0
-# array = []
+Sum = 0
+array = []
 
-# total_mass_s2 = mass_sum(mass_s2_s) + mass_sum(mass_s2_d) + mass_sum(mass_s2_g)
+# Calculating the total mass contained within the stellar, dark matter, and gas components
+total_mass_s2 = mass_sum(mass_s2_s) + mass_sum(mass_s2_d) + mass_sum(mass_s2_g)
 
-# print(f'Total mass s1: {total_mass_s1}')
-# print(f'Total mass s2: {total_mass_s2}')
+print(f'Total mass s1: {total_mass_s1}')
+print(f'Total mass s2: {total_mass_s2}')
 
-# #Question 3
-# plt.figure(figsize=(7.5,10.5))
-# pynbody.plot.image(s1.s, threaded=False)
-# # plt.savefig(f'Plots/Figure 1.png', dpi=800)
-# # plt.clf
+#Question 3
+# Rendering a density heat map and optical image of the face-on image of the stellar component
+plt.figure(figsize=(7.5,10.5))
+pynbody.plot.image(s1.s, threaded=False)
+plt.title('Denisty Heat map of system 1, face on view')
+plt.savefig(f'Plots/Figure 1.png', dpi=800)
+plt.clf
 
-# plt.figure(figsize=(7.5,10.5))
-# pynbody.plot.stars.render(s1.s)
-# # plt.savefig(f'Plots/Figure 2.png', dpi=800)
-# # plt.clf
+plt.figure(figsize=(7.5,10.5))
+pynbody.plot.stars.render(s1.s)
+plt.title('Optical image of system 1, face on view')
+plt.savefig(f'Plots/Figure 2.png', dpi=800)
+plt.clf
 
-# plt.figure(figsize=(7.5,10.5))
-# pynbody.plot.image(s2.s, threaded=False)
-# # plt.savefig(f'Plots/Figure 3.png', dpi=800)
-# # plt.clf
+# Rendering a density heat map and optical image of the face-on image of the stellar component
+plt.figure(figsize=(7.5,10.5))
+pynbody.plot.image(s2.s, threaded=False)
+plt.title('Denisty Heat map of system 2, face on view')
+plt.savefig(f'Plots/Figure 3.png', dpi=800)
+plt.clf
 
-# plt.figure(figsize=(7.5,10.5))
-# pynbody.plot.stars.render(s2.s)
-# # plt.savefig(f'Plots/Figure 4.png', dpi=800)
-# # plt.clf
+plt.figure(figsize=(7.5,10.5))
+pynbody.plot.stars.render(s2.s)
+plt.title('Optical image of system 2, face on view')
+plt.savefig(f'Plots/Figure 4.png', dpi=800)
+plt.clf
 
 
-# #Question 4
-# plt.figure(figsize=(7.5,10.5))
-# pynbody.plot.image(s1.g, threaded=False)
-# # plt.savefig(f'Plots/Figure 5.png', dpi=800)
-# # plt.clf
+#Question 4
+# Generating a face-on image of the gaseous component
+plt.figure(figsize=(7.5,10.5))
+pynbody.plot.image(s1.g, threaded=False)
+plt.title('Denisty Heat map of gaseous component of system 1, face on view')
+plt.savefig(f'Plots/Figure 5.png', dpi=800)
+plt.clf
 
-# plt.figure(figsize=(7.5,10.5))
-# pynbody.plot.image(s2.g, threaded=False)
-# # plt.savefig(f'Plots/Figure 6.png', dpi=800)
-# # plt.clf
+# Generating a face-on image of the gaseous component
+plt.figure(figsize=(7.5,10.5))
+pynbody.plot.image(s2.g, threaded=False)
+plt.title('Denisty Heat map of gaseous component of system 2, face on view')
+plt.savefig(f'Plots/Figure 6.png', dpi=800)
+plt.clf
 
-# #Question 6
+#Question 6
+# Generating the stellar radial density profile using a logarithmic scale on the y-axis
 plt.figure(figsize=(7.5,10.5))
 
 plt.rcParams['font.family'] = 'STIXGeneral'
@@ -105,9 +118,10 @@ plt.ylabel(r'$\log{\rho}$')
 plt.semilogy()
 plt.xlabel('Stellar Radii')
 plt.title('Stellar radial density profile of system 1')
-# plt.savefig(f'Plots/Figure 7.png', dpi=800)
-# plt.clf
+plt.savefig(f'Plots/Figure 7.png', dpi=800)
+plt.clf
 
+# Generating the stellar radial density profile using a logarithmic scale on the y-axis
 plt.figure(figsize=(7.5,10.5))
 
 plt.rcParams['font.family'] = 'STIXGeneral'
@@ -119,21 +133,22 @@ plt.minorticks_on()
 plt.grid(visible=True, which='major', linestyle='-')
 plt.grid(visible=True, which='minor', linestyle='--')
 
-p2 = pynbody.analysis.profile.Profile(s2.s, max=40, nbins=250, ndim=3)
+p2 = pynbody.analysis.profile.Profile(s2.s, max=30, nbins=200, ndim=3)
 plt.plot(p2['rbins'], p2['rho'], color='k')
 plt.ylabel(r'$\log{\rho}$')
 plt.semilogy()
 plt.xlabel('Stellar Radii')
 plt.title('Stellar radial density profile of system 2')
-plt.show()
-# plt.savefig(f'Plots/Figure 8.png', dpi=800)
-# plt.clf
+plt.savefig(f'Plots/Figure 8.png', dpi=800)
+plt.clf
 
 #Task2
 
 #Question 1
+# Rotating the barred galaxy so that its bar is aligned with the x-axis
 pynbody.analysis.angmom.sideon(s1)
 pynbody.plot.image(s1.s, threaded=False)
+plt.title('Denisty Heat map of System 1, side-on view')
 plt.savefig(f'Plots/Figure 9.png', dpi=800)
 plt.clf
 
@@ -142,45 +157,95 @@ radius = 4 # Defining the radius to be considered
 centre = (0,0,0) # Defining the center of the galaxy
 
 sphere1 = s1.s[pynbody.filt.Sphere(radius, centre)] # Filtering the stars according to radii
-pynbody.analysis.angmom.sideon(sphere1) # Aligning the data for a side view
+pynbody.analysis.angmom.sideon(sphere1) # Aligning simulation to appear side-on
 
-sph.image(s1.s, width='8 kpc') # Generating a density heat map of the filtered stars
+# Rendering a density heat map of the side-on image of the filtered stars
+sph.image(s1.s, width='8 kpc') 
+plt.title('Denisty Heat map of filtered system 1, side-on view')
 plt.savefig(f'Plots/Figure 10.png', dpi=800)
 plt.clf
-pynbody.plot.stars.render(sphere1, width='8 kpc') # Generating an image of the filtered stars
+
+plt.title('Optical image of filtered system 1, side-on view')
+pynbody.plot.stars.render(sphere1, width='8 kpc') 
 plt.savefig(f'Plots/Figure 11.png', dpi=800)
 plt.clf
 
-sphere2 = s2.s[pynbody.filt.Sphere(radius, centre)] # Filtering the stars according to radii
-pynbody.analysis.angmom.sideon(sphere2)
+pynbody.analysis.angmom.sideon(s2)
 
-sph.image(s2.s, width='8 kpc') # Generating a density heat map of the filtered stars
+sphere2 = s2.s[pynbody.filt.Sphere(radius, centre)] # Filtering the stars according to radii
+pynbody.analysis.angmom.sideon(sphere2) # Aligning simulation to appear side-on
+
+# Rendering a density heat map of the side-on image of the filtered stars
+plt.title('Denisty Heat map of filtered system 2, side-on view')
+sph.image(s2.s, width='8 kpc')
 plt.savefig(f'Plots/Figure 12.png', dpi=800)
 plt.clf
-pynbody.plot.stars.render(sphere2, width='8 kpc') # Generating an image of the filtered stars
+
+plt.title('Optical image of filtered system 2, side-on view')
+pynbody.plot.stars.render(sphere2, width='8 kpc')
 plt.savefig(f'Plots/Figure 13.png', dpi=800)
 plt.clf
 
 #Question 3
 pynbody.analysis.angmom.sideon(s1) # Viewing the galaxy from the side
-sph.image(s1.s, width='1.5 kpc') # Generating density heat map
+
+# Rendering a density heat map and optical image of the side-on image of the galaxy
+plt.title('Denisty Heat map of filtered system 1, side-on view')
+sph.image(s1.s, width='1.5 kpc')
 plt.savefig(f'Plots/Figure 14.png', dpi=800)
 plt.clf
-pynbody.plot.stars.render(s1.s, width='1.5 kpc') # Generating an image, change the width in step of 0.25 from 0 to 1.5
+
+plt.title('Optical image of filtered system 1, side-on view')
+pynbody.plot.stars.render(s1.s, width='1.5 kpc')
 plt.savefig(f'Plots/Figure 15.png', dpi=800)
 plt.clf
 
 pynbody.analysis.angmom.sideon(s2) # Viewing the galaxy from the side
-sph.image(s2.s, width='1.5 kpc') # Generating density heat map
+
+# Rendering a density heat map and optical image of the side-on image of the galaxy
+plt.title('Denisty Heat map of filtered system 2, side-on view')
+sph.image(s2.s, width='1.5 kpc')
 plt.savefig(f'Plots/Figure 16.png', dpi=800)
 plt.clf
-pynbody.plot.stars.render(s2.s, width='1.5 kpc') # Generating an image, change the width in step of 0.25 from 0 to 1.5
+
+plt.title('Optical image of filtered system 2, side-on view')
+pynbody.plot.stars.render(s2.s, width='1.5 kpc')
 plt.savefig(f'Plots/Figure 17.png', dpi=800)
 plt.clf
 
 #Question 4
-rho_s1 = pynbody.analysis.profile.Profile(s1.s, rmin=0, rmax=1.5) # Filtering stars according to their radii, change rmax in steps of 0.25 from 0 to 1.5
+# Creating filters for the different radii
+radius_filter_0 = pynbody.filt.BandPass('pos', '0 kpc', '0.25 kpc')
+radius_filter_1 = pynbody.filt.BandPass('pos', '0.25 kpc', '0.5 kpc')
+radius_filter_2 = pynbody.filt.BandPass('pos', '0.5 kpc', '0.75 kpc')
+radius_filter_3 = pynbody.filt.BandPass('pos', '0.75 kpc', '1 kpc')
+radius_filter_4 = pynbody.filt.BandPass('pos', '1 kpc', '1.25 kpc')
+radius_filter_5 = pynbody.filt.BandPass('pos', '1.25 kpc', '1.5 kpc')
 
+# Filtering stars according to their radii, changing radius in steps of 0.25 from 0 to 1.5
+s1_1_filtered = s1.s[radius_filter_0]
+s1_2_filtered = s1.s[radius_filter_1]
+s1_3_filtered = s1.s[radius_filter_2]
+s1_4_filtered = s1.s[radius_filter_3]
+s1_5_filtered = s1.s[radius_filter_4]
+s1_6_filtered = s1.s[radius_filter_5]
+
+s2_1_filtered = s2.s[radius_filter_0]
+s2_2_filtered = s2.s[radius_filter_1]
+s2_3_filtered = s2.s[radius_filter_2]
+s2_4_filtered = s2.s[radius_filter_3]
+s2_5_filtered = s2.s[radius_filter_4]
+s2_6_filtered = s2.s[radius_filter_5]
+
+# Generating profiles for each filter
+rho_s1_1 = pynbody.analysis.profile.Profile(s1_1_filtered, ndim=3)
+rho_s1_2 = pynbody.analysis.profile.Profile(s1_2_filtered, ndim=3)
+rho_s1_3 = pynbody.analysis.profile.Profile(s1_3_filtered, ndim=3)
+rho_s1_4 = pynbody.analysis.profile.Profile(s1_4_filtered, ndim=3)
+rho_s1_5 = pynbody.analysis.profile.Profile(s1_5_filtered, ndim=3)
+rho_s1_6 = pynbody.analysis.profile.Profile(s1_6_filtered, ndim=3)
+
+# Plotting the generated stellar radial density profile using a logarithmic scale on the y-axis
 plt.figure(figsize=(7.5,10.5))
 
 plt.rcParams['font.family'] = 'STIXGeneral'
@@ -192,16 +257,29 @@ plt.minorticks_on()
 plt.grid(visible=True, which='major', linestyle='-')
 plt.grid(visible=True, which='minor', linestyle='--')
 
-plt.plot(rho_s1['rbins'], np.log(rho_s1['rho']), color='k') # Plotting the radii density profile
-
+plt.plot(rho_s1_1['rbins'], rho_s1_1['rho'], label='0 to 0.25') 
+plt.plot(rho_s1_2['rbins'], rho_s1_2['rho'], label='0.25 to 0.50')
+plt.plot(rho_s1_3['rbins'], rho_s1_3['rho'], label='0.50 to 0.75')
+plt.plot(rho_s1_4['rbins'], rho_s1_4['rho'], label='0.75 to 1.00')
+plt.plot(rho_s1_5['rbins'], rho_s1_5['rho'], label='1.00 to 1.25')
+plt.plot(rho_s1_6['rbins'], rho_s1_6['rho'], label='1.25 to 1.50')
+plt.semilogy()
 plt.ylabel(r'$\log{\rho}$')
 plt.xlabel('Stellar Radii')
 plt.title('Stellar radial density profile of system 1')
+plt.legend()
 plt.savefig(f'Plots/Figure 18.png', dpi=800)
 plt.clf
 
-rho_s2 = pynbody.analysis.profile.Profile(s2.s, rmin=0, rmax=1.5) # Filtering stars according to their radii, change rmax in steps of 0.25 from 0 to 1.5
+# Generating profiles for each filter
+rho_s2_1 = pynbody.analysis.profile.Profile(s2_1_filtered, ndim=3)
+rho_s2_2 = pynbody.analysis.profile.Profile(s2_2_filtered, ndim=3)
+rho_s2_3 = pynbody.analysis.profile.Profile(s2_3_filtered, ndim=3)
+rho_s2_4 = pynbody.analysis.profile.Profile(s2_4_filtered, ndim=3)
+rho_s2_5 = pynbody.analysis.profile.Profile(s2_5_filtered, ndim=3)
+rho_s2_6 = pynbody.analysis.profile.Profile(s2_6_filtered, ndim=3)
 
+# Plotting the generated stellar radial density profile using a logarithmic scale on the y-axis
 plt.figure(figsize=(7.5,10.5))
 
 plt.rcParams['font.family'] = 'STIXGeneral'
@@ -213,19 +291,49 @@ plt.minorticks_on()
 plt.grid(visible=True, which='major', linestyle='-')
 plt.grid(visible=True, which='minor', linestyle='--')
 
-plt.plot(rho_s2['rbins'], np.log(rho_s2['rho']), color='k') # Plotting the radii density profile
-
+plt.plot(rho_s2_1['rbins'], rho_s2_1['rho'], label='0 to 0.25') 
+plt.plot(rho_s2_2['rbins'], rho_s2_2['rho'], label='0.25 to 0.50')
+plt.plot(rho_s2_3['rbins'], rho_s2_3['rho'], label='0.50 to 0.75')
+plt.plot(rho_s2_4['rbins'], rho_s2_4['rho'], label='0.75 to 1.00')
+plt.plot(rho_s2_5['rbins'], rho_s2_5['rho'], label='1.00 to 1.25')
+plt.plot(rho_s2_6['rbins'], rho_s2_6['rho'], label='1.25 to 1.50')
+plt.semilogy()
 plt.ylabel(r'$\log{\rho}$')
 plt.xlabel('Stellar Radii')
 plt.title('Stellar radial density profile of system 2')
+plt.legend()
 plt.savefig(f'Plots/Figure 19.png', dpi=800)
 plt.clf
 
 #Question 5
-radius_filter = pynbody.filt.BandPass('pos', '0.75 kpc', '1 kpc')
-s1_filtered = s1.s[radius_filter]
-s1_p = pynbody.analysis.profile.Profile(s1_filtered, nbins=200)
+# Creating filters for the different heights
+age_filter_1 = pynbody.filt.BandPass('pos', '0.75 kpc', '0.8 kpc')
+age_filter_2 = pynbody.filt.BandPass('pos', '0.8 kpc', '0.85 kpc')
+age_filter_3 = pynbody.filt.BandPass('pos', '0.85 kpc', '0.9 kpc')
+age_filter_4 = pynbody.filt.BandPass('pos', '0.9 kpc', '0.95 kpc')
+age_filter_5 = pynbody.filt.BandPass('pos', '0.95 kpc', '1 kpc')
 
+# Filtering stars according to their radii, changing radius in steps of 0.05 from 0.75 to 1.00
+s1_1_filtered = s1.s[age_filter_1]
+s1_2_filtered = s1.s[age_filter_2]
+s1_3_filtered = s1.s[age_filter_3]
+s1_4_filtered = s1.s[age_filter_4]
+s1_5_filtered = s1.s[age_filter_5]
+
+s2_1_filtered = s2.s[age_filter_1]
+s2_2_filtered = s2.s[age_filter_2]
+s2_3_filtered = s2.s[age_filter_3]
+s2_4_filtered = s2.s[age_filter_4]
+s2_5_filtered = s2.s[age_filter_5]
+
+# Generating profiles for each filter
+s1_p_1 = pynbody.analysis.profile.Profile(s1_1_filtered, nbins=200)
+s1_p_2 = pynbody.analysis.profile.Profile(s1_2_filtered, nbins=200)
+s1_p_3 = pynbody.analysis.profile.Profile(s1_3_filtered, nbins=200)
+s1_p_4 = pynbody.analysis.profile.Profile(s1_4_filtered, nbins=200)
+s1_p_5 = pynbody.analysis.profile.Profile(s1_5_filtered, nbins=200)
+
+# Plotting the generated stellar age density profile using a logarithmic scale on the y-axis
 plt.figure(figsize=(7.5,10.5))
 
 plt.rcParams['font.family'] = 'STIXGeneral'
@@ -237,17 +345,28 @@ plt.minorticks_on()
 plt.grid(visible=True, which='major', linestyle='-')
 plt.grid(visible=True, which='minor', linestyle='--')
 
-plt.clf; plt.plot(s1_p['rbins'], np.log(s1_p['rho']), color ='k')
+plt.plot(s1_p_1['rbins'], s1_p_1['rho'], label='0.75 to 0.80')
+plt.plot(s1_p_2['rbins'], s1_p_2['rho'], label='0.80 to 0.85')
+plt.plot(s1_p_3['rbins'], s1_p_3['rho'], label='0.85 to 0.90')
+plt.plot(s1_p_4['rbins'], s1_p_4['rho'], label='0.90 to 0.95')
+plt.plot(s1_p_5['rbins'], s1_p_5['rho'], label='0.95 to 1.00')
 
+plt.semilogy()
 plt.ylabel(r'$\log{\rho}$')
 plt.xlabel('Stellar Ages')
-plt.title('Stellar radial density profile of system 1')
+plt.title('Stellar age density profile of system 1')
+plt.legend()
 plt.savefig(f'Plots/Figure 20.png', dpi=800)
 plt.clf
 
-s2_filtered = s2.s[radius_filter]
-s2_p = pynbody.analysis.profile.Profile(s2_filtered, nbins=200)
+# Generating profiles for each filter
+s2_p_1 = pynbody.analysis.profile.Profile(s2_1_filtered, nbins=200)
+s2_p_2 = pynbody.analysis.profile.Profile(s2_2_filtered, nbins=200)
+s2_p_3 = pynbody.analysis.profile.Profile(s2_3_filtered, nbins=200)
+s2_p_4 = pynbody.analysis.profile.Profile(s2_4_filtered, nbins=200)
+s2_p_5 = pynbody.analysis.profile.Profile(s2_5_filtered, nbins=200)
 
+# Plotting the generated stellar age density profile using a logarithmic scale on the y-axis
 plt.figure(figsize=(7.5,10.5))
 
 plt.rcParams['font.family'] = 'STIXGeneral'
@@ -259,10 +378,16 @@ plt.minorticks_on()
 plt.grid(visible=True, which='major', linestyle='-')
 plt.grid(visible=True, which='minor', linestyle='--')
 
-plt.clf; plt.plot(s2_p['rbins'], np.log(s2_p['rho']), color ='k')
+plt.plot(s2_p_1['rbins'], s2_p_1['rho'], label='0.75 to 0.80')
+plt.plot(s2_p_2['rbins'], s2_p_2['rho'], label='0.80 to 0.85')
+plt.plot(s2_p_3['rbins'], s2_p_3['rho'], label='0.85 to 0.90')
+plt.plot(s2_p_4['rbins'], s2_p_4['rho'], label='0.90 to 0.95')
+plt.plot(s2_p_5['rbins'], s2_p_5['rho'], label='0.95 to 1.00')
 
+plt.semilogy()
 plt.ylabel(r'$\log{\rho}$')
 plt.xlabel('Stellar Ages')
-plt.title('Stellar radial density profile of system 1')
+plt.title('Stellar age density profile of system 2')
+plt.legend()
 plt.savefig(f'Plots/Figure 21.png', dpi=800)
 plt.clf
